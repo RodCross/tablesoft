2564d46e5cfd07d59bc5187c7aabf2fb7d10dd8f
RodCross <rodrigocl1312@gmail.com>
2020-04-29 17:30:49 -0500

    Remove ticket list from MesaAyuda

--- pixies-tm-model/src/pe/edu/pucp/pixies/model/MesaAyuda.java
+++ pixies-tm-model/src/pe/edu/pucp/pixies/model/MesaAyuda.java
@@ -3,12 +3,10 @@ package pe.edu.pucp.pixies.model;
 import java.util.ArrayList;
 
 public class MesaAyuda {
-    private ArrayList<Ticket> lista_tickets;
     private ArrayList<Equipo> lista_equipos;
     private SupervisorSistema supervisor;
 
     public MesaAyuda() {
-        this.lista_tickets = new ArrayList<Ticket>();
         this.lista_equipos = new ArrayList<Equipo>();
     }
 
@@ -18,10 +16,6 @@ public class MesaAyuda {
 
     public void setSupervisor(SupervisorSistema supervisor) {
         this.supervisor = supervisor;
-    }
-
-    public void agregarCategoria(Ticket ticket) {
-        this.lista_tickets.add(ticket);
     }
 
     public void agregarAgente(Equipo equipo) {

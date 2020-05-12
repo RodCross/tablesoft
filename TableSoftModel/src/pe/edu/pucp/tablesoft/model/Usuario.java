77fe6a35a090b3e1d118bc3608450b8b0c44fb36
404MS <miguelsl1912@gmail.com>
2020-05-12 17:08:24 -0500

    Update clase Usuario
    codigoPUCP pasa a ser String

--- TableSoftModel/src/pe/edu/pucp/tablesoft/model/Usuario.java
+++ TableSoftModel/src/pe/edu/pucp/tablesoft/model/Usuario.java
@@ -1,34 +1,34 @@
 package pe.edu.pucp.tablesoft.model;
 
 public class Usuario {
-    private int codigo_pucp;
+    private String codigoPUCP;
     private int dni;
     private String nombre;
-    private String correo_electronico;
+    private String emailUsuario;
 
-    public Usuario(int codigo_pucp, int dni, String nombre) {
-        this.codigo_pucp = codigo_pucp;
+    public Usuario(String codigo_pucp, int dni, String nombre) {
+        this.codigoPUCP = codigo_pucp;
         this.dni = dni;
         this.nombre = nombre;
     }
 
-    public Usuario(int codigo_pucp, int dni, String nombre, String correo_electronico) {
-        this.codigo_pucp = codigo_pucp;
+    public Usuario(String codigo_pucp, int dni, String nombre, String correo_electronico) {
+        this.codigoPUCP = codigo_pucp;
         this.dni = dni;
         this.nombre = nombre;
-        this.correo_electronico = correo_electronico;
+        this.emailUsuario = correo_electronico;
     }
 
     public Usuario(){
         
     }
     
-    public int getCodigoPucp() {
-        return this.codigo_pucp;
+    public String getCodigoPucp() {
+        return this.codigoPUCP;
     }
 
-    public void setCodigoPucp(int codigo_pucp) {
-        this.codigo_pucp = codigo_pucp;
+    public void setCodigoPucp(String codigo_pucp) {
+        this.codigoPUCP = codigo_pucp;
     }
 
     public int getDni() {
@@ -48,10 +48,10 @@ public class Usuario {
     }
 
     public String getCorreoElectronico() {
-        return this.correo_electronico;
+        return this.emailUsuario;
     }
 
     public void setCorreoElectronico(String correo_electronico) {
-        this.correo_electronico = correo_electronico;
+        this.emailUsuario = correo_electronico;
     }
 }

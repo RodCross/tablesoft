cbfc548fd1d6196cee19529aef64e3d184bed5b0
20170569 <64267377+20170569@users.noreply.github.com>
2020-05-12 19:02:58 -0500

    INSERTAR_URGENCIA testeado

--- TableSoft/src/tablesoft/TableSoft.java
+++ TableSoft/src/tablesoft/TableSoft.java
@@ -1,5 +1,7 @@
 package tablesoft;
-
+import pe.edu.pucp.tablesoft.model.Urgencia;
+import pe.edu.pucp.tablesoft.mysql.UrgenciaMySQL;
+import pe.edu.pucp.tablesoft.dao.UrgenciaDAO;
 /**
  *
  * @author migue
@@ -11,6 +13,19 @@ public class TableSoft {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        UrgenciaDAO daoUrgencia = new UrgenciaMySQL();
+        
+        Urgencia urgencia = new Urgencia("CONSULTA", 72);
+        int result;
+        result = daoUrgencia.insertar(urgencia);
+        if(result==0)
+            System.out.println("Ha ocurrido un error\n");
+        else{
+            System.out.println("Se ha registrado de manera exitosa\n");
+            System.out.println("El id generado para el empleado es " + result);
+            
+        }
+            
     }
     
 }

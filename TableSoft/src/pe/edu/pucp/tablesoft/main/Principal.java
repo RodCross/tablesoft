4796343e57e40df4f33a7279bfddaef6fd9191dd
RodCross <rodrigocl1312@gmail.com>
2020-05-13 03:40:50 -0500

    Prepare main function for tests

--- TableSoft/src/pe/edu/pucp/tablesoft/main/Principal.java
+++ TableSoft/src/pe/edu/pucp/tablesoft/main/Principal.java
@@ -1,14 +1,33 @@
 package pe.edu.pucp.tablesoft.main;
+
 import java.util.ArrayList;
+import pe.edu.pucp.tablesoft.dao.AgenteDAO;
 import pe.edu.pucp.tablesoft.dao.CategoriaDAO;
+import pe.edu.pucp.tablesoft.dao.EmpleadoDAO;
 import pe.edu.pucp.tablesoft.dao.EquipoDAO;
+import pe.edu.pucp.tablesoft.dao.ProveedorDAO;
+import pe.edu.pucp.tablesoft.dao.TicketDAO;
+import pe.edu.pucp.tablesoft.dao.UrgenciaDAO;
+import pe.edu.pucp.tablesoft.dao.UsuarioDAO;
+import pe.edu.pucp.tablesoft.model.Agente;
 import pe.edu.pucp.tablesoft.model.Categoria;
+import pe.edu.pucp.tablesoft.model.Empleado;
 import pe.edu.pucp.tablesoft.model.Equipo;
+import pe.edu.pucp.tablesoft.model.Proveedor;
+import pe.edu.pucp.tablesoft.model.Ticket;
+import pe.edu.pucp.tablesoft.model.Urgencia;
+import pe.edu.pucp.tablesoft.model.Usuario;
+import pe.edu.pucp.tablesoft.mysql.AgenteMySQL;
 import pe.edu.pucp.tablesoft.mysql.CategoriaMySQL;
+import pe.edu.pucp.tablesoft.mysql.EmpleadoMySQL;
 import pe.edu.pucp.tablesoft.mysql.EquipoMySQL;
+import pe.edu.pucp.tablesoft.mysql.ProveedorMySQL;
+import pe.edu.pucp.tablesoft.mysql.TicketMySQL;
+import pe.edu.pucp.tablesoft.mysql.UrgenciaMySQL;
+import pe.edu.pucp.tablesoft.mysql.UsuarioMySQL;
 
 public class Principal {
     public static void main(String[] args) {
-        
+        // Para pruebas
     }
 }

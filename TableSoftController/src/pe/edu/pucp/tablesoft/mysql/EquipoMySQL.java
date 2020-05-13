561de26377de4fe07e6cc6a97c29edbb9c984633
404MS <miguelsl1912@gmail.com>
2020-05-13 09:41:48 -0500

    Update EquipoMySQL.java
    Cambios en nombres de SP, y actualizacion de listar()

--- TableSoftController/src/pe/edu/pucp/tablesoft/mysql/EquipoMySQL.java
+++ TableSoftController/src/pe/edu/pucp/tablesoft/mysql/EquipoMySQL.java
@@ -28,7 +28,7 @@ public class EquipoMySQL implements EquipoDAO{
                     DBManager.user, DBManager.password);
 
             CallableStatement cs = con.prepareCall(
-                    "{call INSERTAR_EQUIPO(?)}");
+                    "{call instar_equipo(?)}");
             cs.registerOutParameter("_ID", java.sql.Types.INTEGER);
             cs.executeUpdate();
             rpta = cs.getInt("_ID");
@@ -59,7 +59,7 @@ public class EquipoMySQL implements EquipoDAO{
                     DBManager.user, DBManager.password);
 
             CallableStatement cs = con.prepareCall(
-                    "{call ELIMINAR_EQUIPO(?)}");
+                    "{call eliminar_equipo(?)}");
             cs.setInt("_ID", idEquipo);
             cs.executeUpdate();
 
@@ -86,44 +86,56 @@ public class EquipoMySQL implements EquipoDAO{
             con = DriverManager.getConnection(DBManager.urlMySQL, 
                     DBManager.user, DBManager.password);
             
-            String sql = "SELECT * FROM equipo";
-            PreparedStatement ps = con.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()) {
-                Equipo equipo = new Equipo();
-                equipo.setEquipoId(rs.getInt("equipo_id"));
-                equipos.add(equipo);
-            }
             
-            for(Equipo e : equipos) {
-                // Para cada equipo buscamos sus agentes y su supervisor
-                sql = "SELECT * FROM AGENTE WHERE equipo_id = " + e.getEquipoId();
-                ps = con.prepareStatement(sql);
-                rs = ps.executeQuery();
-                while(rs.next()) {
-                    // Verificar el rol
+            CallableStatement cs = con.prepareCall(
+                    "{call listar_equipo_agentes()}");
+            ResultSet rs=cs.executeQuery();
+            
+            int equipo_id_anterior = 0, equipo_id_actual;
+            Equipo equipo;
+            while(rs.next()) {
+                equipo_id_actual = rs.getInt("equipo_id");
+                if(equipo_id_actual != equipo_id_anterior){
+                    equipo = new Equipo();
+                    equipos.add(equipo);
+                }
+                try{
+                    equipo.setEquipoId(equipo_id_actual);
                     String rol = rs.getString("rol");
-                    if("Supervisor".equals(rol)) {
+                    if("Supervisor".equals(rol)){
                         Supervisor sup = new Supervisor();
                         sup.setAgenteEmail(rs.getString("agente_email"));
                         sup.setAgenteId(rs.getInt("agente_id"));
                         sup.setCodigo(rs.getString("codigo"));
-                        sup.setEquipo(e);
-                        // FALTA: asignar nombre, dni y correoUsuario
-                        e.asignarSupervisor(sup);
+                        sup.setDni(rs.getString("dni"));
+                        sup.setUsuarioEmail(rs.getString("usuario_email"));
+                        sup.setNombre(rs.getString("nombre"));
+                        sup.setEquipo(equipo);
+                        equipo.asignarSupervisor(sup);
                     }
-                    else if("Agente".equals(rol)) {
+                    if("Agente".equals(rol)){
                         Agente ag = new Agente();
                         ag.setAgenteEmail(rs.getString("agente_email"));
                         ag.setAgenteId(rs.getInt("agente_id"));
                         ag.setCodigo(rs.getString("codigo"));
-                        ag.setEquipo(e);
-                        // FALTA: asignar nombre, dni y correoUsuario
-                        e.agregarAgente(ag);
+                        ag.setEquipo(equipo);
+                        ag.setDni(rs.getString("dni"));
+                        ag.setUsuarioEmail(rs.getString("usuario_email"));
+                        ag.setNombre(rs.getString("nombre"));
+                        equipo.agregarAgente(ag);
                     }
+                    
+                } catch(Exception ex){
+                    System.out.println(ex.getMessage());
                 }
+                equipo_id_anterior = equipo_id_actual;
+            }
+            
+            for(Equipo e : equipos) {
                 // Para cada equipo buscamos sus categorias
+                String sql;
                 sql = "SELECT * FROM CATEGORIA WHERE equipo_id = " + e.getEquipoId();
+                PreparedStatement ps;
                 ps = con.prepareStatement(sql);
                 rs = ps.executeQuery();
                 while(rs.next()) {

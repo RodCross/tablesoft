372b5a93251b5d4228680c2c9f20ec2c15342d87
20170569 <64267377+20170569@users.noreply.github.com>
2020-05-12 20:15:51 -0500

    MAIN REALIZADO

--- TableSoft/src/tablesoft/TableSoft.java
+++ TableSoft/src/tablesoft/TableSoft.java
@@ -13,13 +13,13 @@ public class TableSoft {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        UrgenciaDAO daoUrgencia = new UrgenciaMySQL();
+        //UrgenciaDAO daoUrgencia = new UrgenciaMySQL();
         
-        Urgencia urgencia = new Urgencia(5, "ELIMINABLE", 89);
+        /*Urgencia urgencia = new Urgencia(5, "ELIMINABLE", 89);
         int codigo = 5;
         int result;
         
-        result = daoUrgencia.eliminar(codigo);   
+        result = daoUrgencia.eliminar(codigo);   */
         //result = daoUrgencia.actualizar(urgencia);   
         //result = daoUrgencia.insertar(urgencia);
         /*if(result==0)
@@ -36,11 +36,12 @@ public class TableSoft {
             System.out.println("Se ha actualizado de manera exitosa\n");            
         }*/
          
-        if(result==0)
+        /*if(result==0)
             System.out.println("Ha ocurrido un error\n");
-        else{
+        else{   
             System.out.println("Se ha eliminado de manera exitosa\n");            
-        }
+        }*/
+        
     }
     
 }

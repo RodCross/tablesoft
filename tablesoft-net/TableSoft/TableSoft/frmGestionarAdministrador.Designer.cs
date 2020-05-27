19cfcd59d4b66efc0313209bebcb5a58f2ceece9
StefanoRH834 <stefanoroldan@hotmail.com>
2020-05-26 21:28:46 -0500

    Esta listo el gestinar administrador

---
+++ tablesoft-net/TableSoft/TableSoft/frmGestionarAdministrador.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TableSoft
+{
+    partial class frmGestionarAdministrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.lblAbrir = new System.Windows.Forms.Label();
+            this.btnGestionarEmpleado = new System.Windows.Forms.Button();
+            this.btnGestionarAgentes = new System.Windows.Forms.Button();
+            this.btnGestionarCategorias = new System.Windows.Forms.Button();
+            this.btnGestionarProveedores = new System.Windows.Forms.Button();
+            this.btnGestionarTareas = new System.Windows.Forms.Button();
+            this.btnGestionarBibliotecas = new System.Windows.Forms.Button();
+            this.btnGestionarUrgencias = new System.Windows.Forms.Button();
+            this.btnGestionarActivosFijos = new System.Windows.Forms.Button();
+            this.btnGestionarEstadosTicket = new System.Windows.Forms.Button();
+            this.pnlTitulo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTitulo
+            // 
+            this.pnlTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(77)))), ((int)(((byte)(115)))));
+            this.pnlTitulo.Controls.Add(this.lblAbrir);
+            this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitulo.Name = "pnlTitulo";
+            this.pnlTitulo.Size = new System.Drawing.Size(1076, 70);
+            this.pnlTitulo.TabIndex = 5;
+            // 
+            // lblAbrir
+            // 
+            this.lblAbrir.AutoSize = true;
+            this.lblAbrir.Font = new System.Drawing.Font("Microsoft PhagsPa", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblAbrir.ForeColor = System.Drawing.Color.White;
+            this.lblAbrir.Location = new System.Drawing.Point(30, 23);
+            this.lblAbrir.Name = "lblAbrir";
+            this.lblAbrir.Size = new System.Drawing.Size(271, 27);
+            this.lblAbrir.TabIndex = 0;
+            this.lblAbrir.Text = "Gestion del Administrador";
+            this.lblAbrir.Click += new System.EventHandler(this.lblAbrir_Click);
+            // 
+            // btnGestionarEmpleado
+            // 
+            this.btnGestionarEmpleado.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGestionarEmpleado.Location = new System.Drawing.Point(285, 89);
+            this.btnGestionarEmpleado.Name = "btnGestionarEmpleado";
+            this.btnGestionarEmpleado.Size = new System.Drawing.Size(498, 42);
+            this.btnGestionarEmpleado.TabIndex = 6;
+            this.btnGestionarEmpleado.Text = "Gestionar Empleados";
+            this.btnGestionarEmpleado.UseVisualStyleBackColor = true;
+            // 
+            // btnGestionarAgentes
+            // 
+            this.btnGestionarAgentes.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold);
+            this.btnGestionarAgentes.Location = new System.Drawing.Point(285, 150);
+            this.btnGestionarAgentes.Name = "btnGestionarAgentes";
+            this.btnGestionarAgentes.Size = new System.Drawing.Size(498, 42);
+            this.btnGestionarAgentes.TabIndex = 7;
+            this.btnGestionarAgentes.Text = "Gestionar Agentes";
+            this.btnGestionarAgentes.UseVisualStyleBackColor = true;
+            // 
+            // btnGestionarCategorias
+            // 
+            this.btnGestionarCategorias.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold);
+            this.btnGestionarCategorias.Location = new System.Drawing.Point(285, 207);
+            this.btnGestionarCategorias.Name = "btnGestionarCategorias";
+            this.btnGestionarCategorias.Size = new System.Drawing.Size(498, 42);
+            this.btnGestionarCategorias.TabIndex = 8;
+            this.btnGestionarCategorias.Text = "Gestionar Categorias";
+            this.btnGestionarCategorias.UseVisualStyleBackColor = true;
+            this.btnGestionarCategorias.Click += new System.EventHandler(this.btnGestionarCategorias_Click);
+            // 
+            // btnGestionarProveedores
+            // 
+            this.btnGestionarProveedores.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold);
+            this.btnGestionarProveedores.Location = new System.Drawing.Point(284, 267);
+            this.btnGestionarProveedores.Name = "btnGestionarProveedores";
+            this.btnGestionarProveedores.Size = new System.Drawing.Size(499, 42);
+            this.btnGestionarProveedores.TabIndex = 9;
+            this.btnGestionarProveedores.Text = "Gestionar Proveedores";
+            this.btnGestionarProveedores.UseVisualStyleBackColor = true;
+            // 
+            // btnGestionarTareas
+            // 
+            this.btnGestionarTareas.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold);
+            this.btnGestionarTareas.Location = new System.Drawing.Point(285, 325);
+            this.btnGestionarTareas.Name = "btnGestionarTareas";
+            this.btnGestionarTareas.Size = new System.Drawing.Size(498, 42);
+            this.btnGestionarTareas.TabIndex = 10;
+            this.btnGestionarTareas.Text = "Gestionar Tareas";
+            this.btnGestionarTareas.UseVisualStyleBackColor = true;
+            // 
+            // btnGestionarBibliotecas
+            // 
+            this.btnGestionarBibliotecas.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold);
+            this.btnGestionarBibliotecas.Location = new System.Drawing.Point(284, 385);
+            this.btnGestionarBibliotecas.Name = "btnGestionarBibliotecas";
+            this.btnGestionarBibliotecas.Size = new System.Drawing.Size(497, 42);
+            this.btnGestionarBibliotecas.TabIndex = 11;
+            this.btnGestionarBibliotecas.Text = "Gestionar Bibliotecas";
+            this.btnGestionarBibliotecas.UseVisualStyleBackColor = true;
+            // 
+            // btnGestionarUrgencias
+            // 
+            this.btnGestionarUrgencias.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold);
+            this.btnGestionarUrgencias.Location = new System.Drawing.Point(284, 443);
+            this.btnGestionarUrgencias.Name = "btnGestionarUrgencias";
+            this.btnGestionarUrgencias.Size = new System.Drawing.Size(497, 42);
+            this.btnGestionarUrgencias.TabIndex = 12;
+            this.btnGestionarUrgencias.Text = "Gestionar Urgencias";
+            this.btnGestionarUrgencias.UseVisualStyleBackColor = true;
+            // 
+            // btnGestionarActivosFijos
+            // 
+            this.btnGestionarActivosFijos.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold);
+            this.btnGestionarActivosFijos.Location = new System.Drawing.Point(285, 501);
+            this.btnGestionarActivosFijos.Name = "btnGestionarActivosFijos";
+            this.btnGestionarActivosFijos.Size = new System.Drawing.Size(497, 42);
+            this.btnGestionarActivosFijos.TabIndex = 13;
+            this.btnGestionarActivosFijos.Text = "Gestionar Activos Fijos";
+            this.btnGestionarActivosFijos.UseVisualStyleBackColor = true;
+            // 
+            // btnGestionarEstadosTicket
+            // 
+            this.btnGestionarEstadosTicket.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Bold);
+            this.btnGestionarEstadosTicket.Location = new System.Drawing.Point(285, 559);
+            this.btnGestionarEstadosTicket.Name = "btnGestionarEstadosTicket";
+            this.btnGestionarEstadosTicket.Size = new System.Drawing.Size(497, 42);
+            this.btnGestionarEstadosTicket.TabIndex = 14;
+            this.btnGestionarEstadosTicket.Text = "Gestionar Estados de Ticket";
+            this.btnGestionarEstadosTicket.UseVisualStyleBackColor = true;
+            // 
+            // frmGestionarAdministrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 633);
+            this.Controls.Add(this.btnGestionarEstadosTicket);
+            this.Controls.Add(this.btnGestionarActivosFijos);
+            this.Controls.Add(this.btnGestionarUrgencias);
+            this.Controls.Add(this.btnGestionarBibliotecas);
+            this.Controls.Add(this.btnGestionarTareas);
+            this.Controls.Add(this.btnGestionarProveedores);
+            this.Controls.Add(this.btnGestionarCategorias);
+            this.Controls.Add(this.btnGestionarAgentes);
+            this.Controls.Add(this.btnGestionarEmpleado);
+            this.Controls.Add(this.pnlTitulo);
+            this.Name = "frmGestionarAdministrador";
+            this.Text = "frmGestionarAdministrador";
+            this.pnlTitulo.ResumeLayout(false);
+            this.pnlTitulo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.Label lblAbrir;
+        private System.Windows.Forms.Button btnGestionarEmpleado;
+        private System.Windows.Forms.Button btnGestionarAgentes;
+        private System.Windows.Forms.Button btnGestionarCategorias;
+        private System.Windows.Forms.Button btnGestionarProveedores;
+        private System.Windows.Forms.Button btnGestionarTareas;
+        private System.Windows.Forms.Button btnGestionarBibliotecas;
+        private System.Windows.Forms.Button btnGestionarUrgencias;
+        private System.Windows.Forms.Button btnGestionarActivosFijos;
+        private System.Windows.Forms.Button btnGestionarEstadosTicket;
+    }
+}
---
+++ tablesoft-net/TableSoft/TableSoft/frmGestionarAdministrador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableSoft
+{
+    public partial class frmGestionarAdministrador : Form
+    {
+        public frmGestionarAdministrador()
+        {
+            InitializeComponent();
+        }
+
+        private void lblAbrir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGestionarCategorias_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

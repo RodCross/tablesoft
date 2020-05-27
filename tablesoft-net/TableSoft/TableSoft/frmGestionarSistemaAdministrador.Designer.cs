f8bd5c79720596080557b855452bcb08a4e12983
PeterThonks <64267514+PeterThonks@users.noreply.github.com>
2020-05-26 22:31:32 -0500

    Modif Gest Sist

--- tablesoft-net/TableSoft/TableSoft/frmGestionarSistemaAdministrador.Designer.cs
+++ tablesoft-net/TableSoft/TableSoft/frmGestionarSistemaAdministrador.Designer.cs
@@ -37,25 +37,19 @@
             this.btnGestionarUrgencias = new System.Windows.Forms.Button();
             this.btnGestionarActivosFijos = new System.Windows.Forms.Button();
             this.btnGestionarEstadosTicket = new System.Windows.Forms.Button();
-            this.pnlOpciones = new System.Windows.Forms.Panel();
-            this.lklLogout = new System.Windows.Forms.LinkLabel();
-            this.pnlTitulo = new System.Windows.Forms.Panel();
-            this.lklFAQ = new System.Windows.Forms.LinkLabel();
-            this.lblBibliotecasPUCP = new System.Windows.Forms.Label();
-            this.lblTituloSistema = new System.Windows.Forms.Label();
-            this.lklTickets = new System.Windows.Forms.LinkLabel();
             this.pnlCaracteristicas = new System.Windows.Forms.Panel();
-            this.lblAdministrarPersonas = new System.Windows.Forms.Label();
             this.btnGestionarSupervisores = new System.Windows.Forms.Button();
+            this.lblAdministrarPersonas = new System.Windows.Forms.Label();
             this.lblAdministrarNegocio = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblAdministrarOtros = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pnlOpciones.SuspendLayout();
-            this.pnlTitulo.SuspendLayout();
+            this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.lblGestionarSistema = new System.Windows.Forms.Label();
             this.pnlCaracteristicas.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.pnlTitulo.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnGestionarEmpleado
@@ -139,119 +133,16 @@
             this.btnGestionarEstadosTicket.Text = "Gestionar Estados de Ticket";
             this.btnGestionarEstadosTicket.UseVisualStyleBackColor = true;
             // 
-            // pnlOpciones
-            // 
-            this.pnlOpciones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(162)))), ((int)(((byte)(208)))));
-            this.pnlOpciones.Controls.Add(this.lklTickets);
-            this.pnlOpciones.Controls.Add(this.lklLogout);
-            this.pnlOpciones.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlOpciones.Location = new System.Drawing.Point(0, 106);
-            this.pnlOpciones.Name = "pnlOpciones";
-            this.pnlOpciones.Size = new System.Drawing.Size(974, 51);
-            this.pnlOpciones.TabIndex = 16;
-            // 
-            // lklLogout
-            // 
-            this.lklLogout.ActiveLinkColor = System.Drawing.Color.Yellow;
-            this.lklLogout.AutoSize = true;
-            this.lklLogout.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lklLogout.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.lklLogout.LinkColor = System.Drawing.Color.White;
-            this.lklLogout.Location = new System.Drawing.Point(820, 12);
-            this.lklLogout.Name = "lklLogout";
-            this.lklLogout.Size = new System.Drawing.Size(128, 25);
-            this.lklLogout.TabIndex = 2;
-            this.lklLogout.TabStop = true;
-            this.lklLogout.Text = "Cerrar sesión";
-            this.lklLogout.VisitedLinkColor = System.Drawing.Color.White;
-            // 
-            // pnlTitulo
-            // 
-            this.pnlTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(77)))), ((int)(((byte)(115)))));
-            this.pnlTitulo.Controls.Add(this.lklFAQ);
-            this.pnlTitulo.Controls.Add(this.lblBibliotecasPUCP);
-            this.pnlTitulo.Controls.Add(this.lblTituloSistema);
-            this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlTitulo.Location = new System.Drawing.Point(0, 0);
-            this.pnlTitulo.Name = "pnlTitulo";
-            this.pnlTitulo.Size = new System.Drawing.Size(974, 106);
-            this.pnlTitulo.TabIndex = 15;
-            // 
-            // lklFAQ
-            // 
-            this.lklFAQ.ActiveLinkColor = System.Drawing.Color.Yellow;
-            this.lklFAQ.AutoSize = true;
-            this.lklFAQ.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Bold);
-            this.lklFAQ.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.lklFAQ.LinkColor = System.Drawing.Color.White;
-            this.lklFAQ.Location = new System.Drawing.Point(810, 65);
-            this.lklFAQ.Name = "lklFAQ";
-            this.lklFAQ.Size = new System.Drawing.Size(138, 17);
-            this.lklFAQ.TabIndex = 0;
-            this.lklFAQ.TabStop = true;
-            this.lklFAQ.Text = "Preguntas frecuentes";
-            this.lklFAQ.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.lklFAQ.VisitedLinkColor = System.Drawing.Color.White;
-            // 
-            // lblBibliotecasPUCP
-            // 
-            this.lblBibliotecasPUCP.AutoSize = true;
-            this.lblBibliotecasPUCP.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.5F);
-            this.lblBibliotecasPUCP.ForeColor = System.Drawing.Color.White;
-            this.lblBibliotecasPUCP.Location = new System.Drawing.Point(24, 59);
-            this.lblBibliotecasPUCP.Name = "lblBibliotecasPUCP";
-            this.lblBibliotecasPUCP.Size = new System.Drawing.Size(159, 26);
-            this.lblBibliotecasPUCP.TabIndex = 0;
-            this.lblBibliotecasPUCP.Text = "Bibliotecas PUCP";
-            // 
-            // lblTituloSistema
-            // 
-            this.lblTituloSistema.AutoSize = true;
-            this.lblTituloSistema.Font = new System.Drawing.Font("Microsoft PhagsPa", 19.25F, System.Drawing.FontStyle.Bold);
-            this.lblTituloSistema.ForeColor = System.Drawing.Color.White;
-            this.lblTituloSistema.Location = new System.Drawing.Point(23, 17);
-            this.lblTituloSistema.Name = "lblTituloSistema";
-            this.lblTituloSistema.Size = new System.Drawing.Size(340, 34);
-            this.lblTituloSistema.TabIndex = 0;
-            this.lblTituloSistema.Text = "Sistema de Mesa de Ayuda";
-            // 
-            // lklTickets
-            // 
-            this.lklTickets.ActiveLinkColor = System.Drawing.Color.Yellow;
-            this.lklTickets.AutoSize = true;
-            this.lklTickets.Enabled = false;
-            this.lklTickets.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lklTickets.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
-            this.lklTickets.LinkColor = System.Drawing.Color.White;
-            this.lklTickets.Location = new System.Drawing.Point(683, 12);
-            this.lklTickets.Name = "lklTickets";
-            this.lklTickets.Size = new System.Drawing.Size(105, 25);
-            this.lklTickets.TabIndex = 1;
-            this.lklTickets.TabStop = true;
-            this.lklTickets.Text = "Mis tickets";
-            this.lklTickets.VisitedLinkColor = System.Drawing.Color.White;
-            // 
             // pnlCaracteristicas
             // 
             this.pnlCaracteristicas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlCaracteristicas.Controls.Add(this.btnGestionarSupervisores);
             this.pnlCaracteristicas.Controls.Add(this.btnGestionarEmpleado);
             this.pnlCaracteristicas.Controls.Add(this.btnGestionarAgentes);
-            this.pnlCaracteristicas.Location = new System.Drawing.Point(29, 233);
+            this.pnlCaracteristicas.Location = new System.Drawing.Point(35, 143);
             this.pnlCaracteristicas.Name = "pnlCaracteristicas";
             this.pnlCaracteristicas.Size = new System.Drawing.Size(263, 200);
             this.pnlCaracteristicas.TabIndex = 17;
-            // 
-            // lblAdministrarPersonas
-            // 
-            this.lblAdministrarPersonas.AutoSize = true;
-            this.lblAdministrarPersonas.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAdministrarPersonas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
-            this.lblAdministrarPersonas.Location = new System.Drawing.Point(24, 190);
-            this.lblAdministrarPersonas.Name = "lblAdministrarPersonas";
-            this.lblAdministrarPersonas.Size = new System.Drawing.Size(202, 25);
-            this.lblAdministrarPersonas.TabIndex = 18;
-            this.lblAdministrarPersonas.Text = "Administrar Personas";
             // 
             // btnGestionarSupervisores
             // 
@@ -263,12 +154,23 @@
             this.btnGestionarSupervisores.Text = "Gestionar Supervisores";
             this.btnGestionarSupervisores.UseVisualStyleBackColor = true;
             // 
+            // lblAdministrarPersonas
+            // 
+            this.lblAdministrarPersonas.AutoSize = true;
+            this.lblAdministrarPersonas.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdministrarPersonas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.lblAdministrarPersonas.Location = new System.Drawing.Point(30, 100);
+            this.lblAdministrarPersonas.Name = "lblAdministrarPersonas";
+            this.lblAdministrarPersonas.Size = new System.Drawing.Size(202, 25);
+            this.lblAdministrarPersonas.TabIndex = 18;
+            this.lblAdministrarPersonas.Text = "Administrar Personas";
+            // 
             // lblAdministrarNegocio
             // 
             this.lblAdministrarNegocio.AutoSize = true;
             this.lblAdministrarNegocio.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAdministrarNegocio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
-            this.lblAdministrarNegocio.Location = new System.Drawing.Point(358, 190);
+            this.lblAdministrarNegocio.Location = new System.Drawing.Point(364, 100);
             this.lblAdministrarNegocio.Name = "lblAdministrarNegocio";
             this.lblAdministrarNegocio.Size = new System.Drawing.Size(197, 25);
             this.lblAdministrarNegocio.TabIndex = 19;
@@ -280,7 +182,7 @@
             this.panel1.Controls.Add(this.btnGestionarCategorias);
             this.panel1.Controls.Add(this.btnGestionarUrgencias);
             this.panel1.Controls.Add(this.btnGestionarEstadosTicket);
-            this.panel1.Location = new System.Drawing.Point(363, 233);
+            this.panel1.Location = new System.Drawing.Point(369, 143);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(263, 200);
             this.panel1.TabIndex = 18;
@@ -290,7 +192,7 @@
             this.lblAdministrarOtros.AutoSize = true;
             this.lblAdministrarOtros.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAdministrarOtros.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
-            this.lblAdministrarOtros.Location = new System.Drawing.Point(680, 190);
+            this.lblAdministrarOtros.Location = new System.Drawing.Point(686, 100);
             this.lblAdministrarOtros.Name = "lblAdministrarOtros";
             this.lblAdministrarOtros.Size = new System.Drawing.Size(171, 25);
             this.lblAdministrarOtros.TabIndex = 21;
@@ -302,35 +204,53 @@
             this.panel2.Controls.Add(this.btnGestionarProveedores);
             this.panel2.Controls.Add(this.btnGestionarActivosFijos);
             this.panel2.Controls.Add(this.btnGestionarBibliotecas);
-            this.panel2.Location = new System.Drawing.Point(685, 233);
+            this.panel2.Location = new System.Drawing.Point(691, 143);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(263, 200);
             this.panel2.TabIndex = 20;
+            // 
+            // pnlTitulo
+            // 
+            this.pnlTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(77)))), ((int)(((byte)(115)))));
+            this.pnlTitulo.Controls.Add(this.lblGestionarSistema);
+            this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitulo.Name = "pnlTitulo";
+            this.pnlTitulo.Size = new System.Drawing.Size(974, 70);
+            this.pnlTitulo.TabIndex = 22;
+            // 
+            // lblGestionarSistema
+            // 
+            this.lblGestionarSistema.AutoSize = true;
+            this.lblGestionarSistema.Font = new System.Drawing.Font("Microsoft PhagsPa", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblGestionarSistema.ForeColor = System.Drawing.Color.White;
+            this.lblGestionarSistema.Location = new System.Drawing.Point(30, 23);
+            this.lblGestionarSistema.Name = "lblGestionarSistema";
+            this.lblGestionarSistema.Size = new System.Drawing.Size(188, 27);
+            this.lblGestionarSistema.TabIndex = 0;
+            this.lblGestionarSistema.Text = "Gestionar Sistema";
             // 
             // frmInicioAdministrador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(974, 449);
+            this.ClientSize = new System.Drawing.Size(974, 358);
+            this.Controls.Add(this.pnlTitulo);
             this.Controls.Add(this.lblAdministrarOtros);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblAdministrarNegocio);
             this.Controls.Add(this.lblAdministrarPersonas);
             this.Controls.Add(this.pnlCaracteristicas);
-            this.Controls.Add(this.pnlOpciones);
-            this.Controls.Add(this.pnlTitulo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmInicioAdministrador";
             this.Text = "frmGestionarAdministrador";
-            this.pnlOpciones.ResumeLayout(false);
-            this.pnlOpciones.PerformLayout();
-            this.pnlTitulo.ResumeLayout(false);
-            this.pnlTitulo.PerformLayout();
             this.pnlCaracteristicas.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.pnlTitulo.ResumeLayout(false);
+            this.pnlTitulo.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -345,13 +265,6 @@
         private System.Windows.Forms.Button btnGestionarUrgencias;
         private System.Windows.Forms.Button btnGestionarActivosFijos;
         private System.Windows.Forms.Button btnGestionarEstadosTicket;
-        private System.Windows.Forms.Panel pnlOpciones;
-        private System.Windows.Forms.LinkLabel lklTickets;
-        private System.Windows.Forms.LinkLabel lklLogout;
-        private System.Windows.Forms.Panel pnlTitulo;
-        private System.Windows.Forms.LinkLabel lklFAQ;
-        private System.Windows.Forms.Label lblBibliotecasPUCP;
-        private System.Windows.Forms.Label lblTituloSistema;
         private System.Windows.Forms.Panel pnlCaracteristicas;
         private System.Windows.Forms.Label lblAdministrarPersonas;
         private System.Windows.Forms.Button btnGestionarSupervisores;
@@ -359,5 +272,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblAdministrarOtros;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.Label lblGestionarSistema;
     }
 }

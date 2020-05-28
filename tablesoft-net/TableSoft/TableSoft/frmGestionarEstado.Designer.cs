fb35a2ce20df8c7c9fdb47463d09da8979ff9824
20170569 <64267377+20170569@users.noreply.github.com>
2020-05-27 20:59:28 -0500

    Seleccionar Estado añadido

--- tablesoft-net/TableSoft/TableSoft/frmGestionarEstado.Designer.cs
+++ tablesoft-net/TableSoft/TableSoft/frmGestionarEstado.Designer.cs
@@ -35,11 +35,15 @@
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.gpbDatosGenerales = new System.Windows.Forms.GroupBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
             this.lblNombre = new System.Windows.Forms.Label();
-            this.lblIDEmpleado = new System.Windows.Forms.Label();
-            this.txtIDEmpleado = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblIDAgente = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtIDAgente = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
             this.pnlTitulo.SuspendLayout();
+            this.gpbDatosGenerales.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblGestionarEstados
@@ -60,7 +64,7 @@
             this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBorrar.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBorrar.ForeColor = System.Drawing.Color.White;
-            this.btnBorrar.Location = new System.Drawing.Point(28, 220);
+            this.btnBorrar.Location = new System.Drawing.Point(303, 234);
             this.btnBorrar.Name = "btnBorrar";
             this.btnBorrar.Size = new System.Drawing.Size(102, 33);
             this.btnBorrar.TabIndex = 25;
@@ -74,7 +78,7 @@
             this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnActualizar.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnActualizar.ForeColor = System.Drawing.Color.White;
-            this.btnActualizar.Location = new System.Drawing.Point(136, 220);
+            this.btnActualizar.Location = new System.Drawing.Point(411, 234);
             this.btnActualizar.Name = "btnActualizar";
             this.btnActualizar.Size = new System.Drawing.Size(102, 33);
             this.btnActualizar.TabIndex = 24;
@@ -88,7 +92,7 @@
             this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGuardar.ForeColor = System.Drawing.Color.White;
-            this.btnGuardar.Location = new System.Drawing.Point(244, 220);
+            this.btnGuardar.Location = new System.Drawing.Point(519, 234);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(102, 33);
             this.btnGuardar.TabIndex = 23;
@@ -102,7 +106,7 @@
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(352, 220);
+            this.btnCancelar.Location = new System.Drawing.Point(627, 234);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(102, 33);
             this.btnCancelar.TabIndex = 22;
@@ -117,58 +121,89 @@
             this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTitulo.Location = new System.Drawing.Point(0, 0);
             this.pnlTitulo.Name = "pnlTitulo";
-            this.pnlTitulo.Size = new System.Drawing.Size(483, 70);
+            this.pnlTitulo.Size = new System.Drawing.Size(741, 70);
             this.pnlTitulo.TabIndex = 21;
             this.pnlTitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTitulo_MouseDown);
+            // 
+            // gpbDatosGenerales
+            // 
+            this.gpbDatosGenerales.Controls.Add(this.txtNombre);
+            this.gpbDatosGenerales.Controls.Add(this.lblNombre);
+            this.gpbDatosGenerales.Controls.Add(this.lblIDAgente);
+            this.gpbDatosGenerales.Controls.Add(this.txtCodigo);
+            this.gpbDatosGenerales.Controls.Add(this.txtIDAgente);
+            this.gpbDatosGenerales.Controls.Add(this.lblCodigo);
+            this.gpbDatosGenerales.Font = new System.Drawing.Font("Microsoft PhagsPa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbDatosGenerales.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.gpbDatosGenerales.Location = new System.Drawing.Point(35, 76);
+            this.gpbDatosGenerales.Name = "gpbDatosGenerales";
+            this.gpbDatosGenerales.Size = new System.Drawing.Size(694, 140);
+            this.gpbDatosGenerales.TabIndex = 26;
+            this.gpbDatosGenerales.TabStop = false;
+            this.gpbDatosGenerales.Text = "Datos Generales";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(151, 60);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(160, 26);
+            this.txtNombre.TabIndex = 15;
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F);
-            this.lblNombre.Location = new System.Drawing.Point(47, 151);
+            this.lblNombre.Location = new System.Drawing.Point(55, 62);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(71, 21);
-            this.lblNombre.TabIndex = 28;
+            this.lblNombre.TabIndex = 9;
             this.lblNombre.Text = "Nombre:";
             // 
-            // lblIDEmpleado
+            // lblIDAgente
             // 
-            this.lblIDEmpleado.AutoSize = true;
-            this.lblIDEmpleado.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIDEmpleado.Location = new System.Drawing.Point(46, 99);
-            this.lblIDEmpleado.Name = "lblIDEmpleado";
-            this.lblIDEmpleado.Size = new System.Drawing.Size(72, 21);
-            this.lblIDEmpleado.TabIndex = 26;
-            this.lblIDEmpleado.Text = "ID Ticket:";
+            this.lblIDAgente.AutoSize = true;
+            this.lblIDAgente.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIDAgente.Location = new System.Drawing.Point(51, 28);
+            this.lblIDAgente.Name = "lblIDAgente";
+            this.lblIDAgente.Size = new System.Drawing.Size(80, 21);
+            this.lblIDAgente.TabIndex = 1;
+            this.lblIDAgente.Text = "ID Equipo:";
             // 
-            // txtIDEmpleado
+            // txtCodigo
             // 
-            this.txtIDEmpleado.Enabled = false;
-            this.txtIDEmpleado.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtIDEmpleado.Location = new System.Drawing.Point(136, 97);
-            this.txtIDEmpleado.Name = "txtIDEmpleado";
-            this.txtIDEmpleado.ReadOnly = true;
-            this.txtIDEmpleado.Size = new System.Drawing.Size(67, 28);
-            this.txtIDEmpleado.TabIndex = 27;
+            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(151, 97);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(521, 26);
+            this.txtCodigo.TabIndex = 4;
             // 
-            // comboBox1
+            // txtIDAgente
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(136, 148);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(210, 29);
-            this.comboBox1.TabIndex = 30;
+            this.txtIDAgente.Enabled = false;
+            this.txtIDAgente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIDAgente.Location = new System.Drawing.Point(151, 23);
+            this.txtIDAgente.Name = "txtIDAgente";
+            this.txtIDAgente.ReadOnly = true;
+            this.txtIDAgente.Size = new System.Drawing.Size(67, 26);
+            this.txtIDAgente.TabIndex = 2;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F);
+            this.lblCodigo.Location = new System.Drawing.Point(28, 99);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(94, 21);
+            this.lblCodigo.TabIndex = 3;
+            this.lblCodigo.Text = "Descripcion:";
             // 
             // frmGestionarEstado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 279);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.lblNombre);
-            this.Controls.Add(this.lblIDEmpleado);
-            this.Controls.Add(this.txtIDEmpleado);
+            this.ClientSize = new System.Drawing.Size(741, 282);
+            this.Controls.Add(this.gpbDatosGenerales);
             this.Controls.Add(this.btnBorrar);
             this.Controls.Add(this.btnActualizar);
             this.Controls.Add(this.btnGuardar);
@@ -180,8 +215,9 @@
             this.Text = "frmGestionarEstado";
             this.pnlTitulo.ResumeLayout(false);
             this.pnlTitulo.PerformLayout();
+            this.gpbDatosGenerales.ResumeLayout(false);
+            this.gpbDatosGenerales.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -192,9 +228,12 @@
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.GroupBox gpbDatosGenerales;
+        private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.Label lblIDEmpleado;
-        private System.Windows.Forms.TextBox txtIDEmpleado;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblIDAgente;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtIDAgente;
+        private System.Windows.Forms.Label lblCodigo;
     }
 }
---
+++ tablesoft-net/TableSoft/TableSoft/frmSeleccionarEstado.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace TableSoft
+{
+    partial class frmSeleccionarEstado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.lblListaUrgencias = new System.Windows.Forms.Label();
+            this.dgvGestionar = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.pnlTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGestionar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTitulo
+            // 
+            this.pnlTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(77)))), ((int)(((byte)(115)))));
+            this.pnlTitulo.Controls.Add(this.lblListaUrgencias);
+            this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitulo.Name = "pnlTitulo";
+            this.pnlTitulo.Size = new System.Drawing.Size(767, 70);
+            this.pnlTitulo.TabIndex = 9;
+            // 
+            // lblListaUrgencias
+            // 
+            this.lblListaUrgencias.AutoSize = true;
+            this.lblListaUrgencias.Font = new System.Drawing.Font("Microsoft PhagsPa", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblListaUrgencias.ForeColor = System.Drawing.Color.White;
+            this.lblListaUrgencias.Location = new System.Drawing.Point(30, 23);
+            this.lblListaUrgencias.Name = "lblListaUrgencias";
+            this.lblListaUrgencias.Size = new System.Drawing.Size(158, 27);
+            this.lblListaUrgencias.TabIndex = 0;
+            this.lblListaUrgencias.Text = "Lista de Estado";
+            // 
+            // dgvGestionar
+            // 
+            this.dgvGestionar.AllowUserToAddRows = false;
+            this.dgvGestionar.AllowUserToDeleteRows = false;
+            this.dgvGestionar.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.dgvGestionar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvGestionar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGestionar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nombre,
+            this.Descripcion});
+            this.dgvGestionar.Location = new System.Drawing.Point(35, 99);
+            this.dgvGestionar.Name = "dgvGestionar";
+            this.dgvGestionar.ReadOnly = true;
+            this.dgvGestionar.Size = new System.Drawing.Size(694, 302);
+            this.dgvGestionar.TabIndex = 10;
+            // 
+            // ID
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F);
+            this.ID.DefaultCellStyle = dataGridViewCellStyle5;
+            this.ID.FillWeight = 50F;
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 50;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 300;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Descripción";
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.ReadOnly = true;
+            this.Descripcion.Width = 300;
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.btnSeleccionar.FlatAppearance.BorderSize = 0;
+            this.btnSeleccionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSeleccionar.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeleccionar.ForeColor = System.Drawing.Color.White;
+            this.btnSeleccionar.Location = new System.Drawing.Point(611, 429);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(119, 33);
+            this.btnSeleccionar.TabIndex = 24;
+            this.btnSeleccionar.Text = "Seleccionar";
+            this.btnSeleccionar.UseVisualStyleBackColor = false;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.btnNuevo.FlatAppearance.BorderSize = 0;
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevo.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.ForeColor = System.Drawing.Color.White;
+            this.btnNuevo.Location = new System.Drawing.Point(466, 429);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(119, 33);
+            this.btnNuevo.TabIndex = 23;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = false;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.btnVolver.FlatAppearance.BorderSize = 0;
+            this.btnVolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVolver.Font = new System.Drawing.Font("Microsoft PhagsPa", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.ForeColor = System.Drawing.Color.White;
+            this.btnVolver.Location = new System.Drawing.Point(319, 429);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(119, 33);
+            this.btnVolver.TabIndex = 22;
+            this.btnVolver.Text = "< Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            // 
+            // frmSeleccionarEstado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 484);
+            this.Controls.Add(this.btnSeleccionar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.dgvGestionar);
+            this.Controls.Add(this.pnlTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSeleccionarEstado";
+            this.Text = "frmSeleccionarEstado";
+            this.pnlTitulo.ResumeLayout(false);
+            this.pnlTitulo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGestionar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.Label lblListaUrgencias;
+        private System.Windows.Forms.DataGridView dgvGestionar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.Button btnSeleccionar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnVolver;
+    }
+}

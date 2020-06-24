263147ce930083d3224069fbc6405cf02ab04b77
404MS <miguelsl1912@gmail.com>
2020-06-24 10:12:04 -0500

    LocalDateTimeAdapter

---
+++ tablesoft-java/TableSoftModel/src/pe/edu/pucp/tablesoft/model/LocalDateTimeAdapter.java
@@ -0,0 +1,21 @@
+package pe.edu.pucp.tablesoft.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/*
+ * @author migue
+ */
+public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime>{
+        @Override
+        public LocalDateTime unmarshal(String v) throws Exception {
+            return LocalDateTime.parse(v,DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
+        @Override
+        public String marshal(LocalDateTime v) throws Exception {
+            return v.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
+    
+    
+}
---
+++ tablesoft-java/TableSoftModel/src/pe/edu/pucp/tablesoft/model/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@XmlJavaTypeAdapters({@XmlJavaTypeAdapter (value = LocalDateTimeAdapter.class)})
+package pe.edu.pucp.tablesoft.model;
+
+import java.time.LocalDateTime;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+

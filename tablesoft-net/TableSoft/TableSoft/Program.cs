774d8172f9033392a06bf21cefb3ce6246bc0248
404MS <miguelsl1912@gmail.com>
2020-06-24 18:02:50 -0500

    Update Program.cs

--- tablesoft-net/TableSoft/TableSoft/Program.cs
+++ tablesoft-net/TableSoft/TableSoft/Program.cs
@@ -13,7 +13,7 @@ namespace TableSoft
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmSeleccionarActivoFijo());
+            Application.Run(new frmGestionarSistemaAdministrador());
         }
     }
 }

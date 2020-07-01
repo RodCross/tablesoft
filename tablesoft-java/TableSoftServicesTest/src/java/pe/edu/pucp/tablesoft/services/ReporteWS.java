14e0c636afbff1300a39354d5f7636bfdc6e8393
StefanoRH834 <stefanoroldan@hotmail.com>
2020-07-01 18:08:42 -0500

    ReporteWebServices pi

---
+++ tablesoft-java/TableSoftServicesTest/src/java/pe/edu/pucp/tablesoft/services/ReporteWS.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.tablesoft.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.swing.ImageIcon;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+import pe.edu.pucp.tablesoft.config.DBManager;
+import pe.edu.pucp.tablesoft.servlets.ReporteTickets;
+
+/**
+ *
+ * @author STEFANO
+ */
+@WebService(serviceName = "ReporteWS")
+public class ReporteWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "generarReporteTickets")
+    public byte[] generarReporteTickets(@WebParam(name = "idTicket") int idTicket) {
+        byte[] arreglo = null;
+        Connection con;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.urlMySQL,DBManager.user, DBManager.password);
+            
+            String rutaImagen
+                = ReporteTickets.class.getResource(
+       "/pe/edu/pucp/tablesoft/reports/Seal_of_Pontifical_Catholic_University_of_Peru.png").getPath();
+        ImageIcon icon = new ImageIcon(rutaImagen);
+        java.awt.Image imagenReporte = icon.getImage();
+
+            String rutaReporteJasper
+            = ReporteTickets.class.getResource(
+           "/pe/edu/pucp/tablesoft/reports/ReporteTicket.jasper").getPath();
+
+            //Obtener la ruta del subreporte
+            String rutaSubReporteJasper
+            = ReporteTickets.class.getResource(
+           "/pe/edu/pucp/tablesoft/reports/Lista_Comentarios_Ticket.jasper").getPath();
+
+            //Obtener la ruta del subreporte2
+            String rutaSubReporte2Jasper
+            = ReporteTickets.class.getResource(
+           "/pe/edu/pucp/tablesoft/reports/Lista_Transferencia_Ticket.jasper").getPath();
+
+            String rutaSubReporte3Jasper
+            = ReporteTickets.class.getResource(
+           "/pe/edu/pucp/tablesoft/reports/Lista_Tareas_Ticket.jasper").getPath();
+
+            //Obtener la ruta al archivo
+            String rutaSubReporte4Jasper
+            = ReporteTickets.class.getResource(
+           "/pe/edu/pucp/tablesoft/reports/Reporte_Lista_Cambios_Estado.jasper").getPath();
+
+            String rutaSubReporte5Jasper
+            = ReporteTickets.class.getResource(
+           "/pe/edu/pucp/tablesoft/reports/Lista_Transferencia_Externa.jasper").getPath();
+
+            //Objecto JasperReport
+            JasperReport objJR = 
+                    (JasperReport)
+                    JRLoader.loadObjectFromFile(rutaReporteJasper);
+
+            HashMap hm = new HashMap();
+
+            hm.put("Ticket_id", idTicket);
+            hm.put("RUTA_LISTA_COMENTARIOS", rutaSubReporteJasper);
+            hm.put("RUTA_LISTA_TRANS_TICKET", rutaSubReporte2Jasper);
+            hm.put("RUTA_LISTA_TAREAS", rutaSubReporte3Jasper);
+            hm.put("RUTA_LISTA_CAMBIOS", rutaSubReporte4Jasper);
+            hm.put("RUTA_LISTA_TRANS_EXTERNA", rutaSubReporte5Jasper);
+            hm.put("IMAGEN_REPORTE", imagenReporte);
+            //Object JasperPrint
+            JasperPrint objJPrint =
+            JasperFillManager.fillReport(objJR,hm,con);
+
+            //Cerramos la conexion
+            con.close();
+
+            arreglo = JasperExportManager.exportReportToPdf(objJPrint);
+        
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return arreglo;
+    }
+}
